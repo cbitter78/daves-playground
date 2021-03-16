15297a7581528823168ba6effebde8e64a133520
Charles Bitter <charles_bitter@cable.comcast.net>
2021-03-15 21:19:41 -0400

    working

--- download.py
+++ download.py
@@ -1,13 +1,19 @@
 #! /usr/bin/env python3
 
 import requests
+import re
+
+pattern = re.compile(r'value=\'([\d-]*)\' name="Id" class="btn btn-link docImage')
 
 
-pdf_id = "01685273-4260742"
 
 
 
-url = f"https://businesssearch.sos.ca.gov/Document/RetrievePDF?Id={pdf_id}"
-r = requests.get(url, verify=False)  
-with open(f"pdfs/{pdf_id}.pdf", 'wb') as f:
-    f.write(r.content)
+
+
+
+for (pdf_id) in re.findall(pattern, web_page_guts):
+    url = f"https://businesssearch.sos.ca.gov/Document/RetrievePDF?Id={pdf_id}"
+    r = requests.get(url, verify=False)  
+    with open(f"pdfs/{pdf_id}.pdf", 'wb') as f:
+        f.write(r.content)
