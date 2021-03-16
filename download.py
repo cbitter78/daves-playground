1e50ec7a3c3d3e536c2e8d7115456a166a8e5ac8
Charles Bitter <charles_bitter@cable.comcast.net>
2021-03-15 21:25:49 -0400

    working

--- download.py
+++ download.py
@@ -6,8 +6,6 @@ import re
 pattern = re.compile(r'value=\'([\d-]*)\' name="Id" class="btn btn-link docImage')
 
 # you get the web page guts
-
-
 for (pdf_id) in re.findall(pattern, web_page_guts):
     url = f"https://businesssearch.sos.ca.gov/Document/RetrievePDF?Id={pdf_id}"
     r = requests.get(url, verify=False)  
